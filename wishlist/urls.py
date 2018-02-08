8fd038bb863a08aa72a204f3275461bd62d60456
Ian Kirkpatrick <ianmann56@gmail.com>
2018-02-08 12:16:53 -0500

    Fixed stuff

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -4,7 +4,10 @@ from ezi.urls import crud_api_url_factory
 
 from wishlist.models import Wishlist, ItemRequest, ItemRecord
 
+from wishlist.views import WishlistIndexView
+
 urlpatterns = [
+    url(r'^index/(?P<pk>\d+)/$', WishlistIndexView.as_view(), name="index"),
 ]
 
 urlpatterns += crud_api_url_factory([
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
 
 from main.utils import ctx_with_settings
+from wishlist.models import Wishlist
+
+class WishlistIndexView(DetailView):
+
+    model = Wishlist
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(WishlistIndexView, self).get_context_data(*args, **kwargs)
+        context["settings"] = settings
+        return context
